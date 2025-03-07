524d376c46aecb64d8be3c0c8754a876f39c792f
WWarrior64 <115961340+WWarrior64@users.noreply.github.com>
2025-03-06 19:34:45 -0600

    Agregar archivos de proyecto.

---
+++ PracticaMVC/Models/marcas.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace PracticaMVC.Models
+{
+	public class marcas
+	{
+		[Key]
+		[DisplayName("ID de Marca")]
+		public int id_marcas { get; set; }
+
+		[DisplayName("Nombre de la marca")]
+		[Required(ErrorMessage = "El nombre de la marca NO es opcional!")]
+		public string nombre_marca { get; set; }
+
+		[DisplayName("Estado")]
+		[StringLength(1, ErrorMessage = "La cantidad maxima de caracteres valida es {1}")]
+		public string? estados { get; set; }
+	}
+}
