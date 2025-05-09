b7880bf32d07c4919edc4d1b4360d4152e93696b
WWarrior64 <115961340+WWarrior64@users.noreply.github.com>
2025-05-09 16:29:57 -0600

    Practica MVC - Envio de correos utilizando SQL Mail y ASP.NET

---
+++ PracticaMVC/Controllers/InicioController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PracticaMVC.Models;
+using PracticaMVC.Services;
+
+namespace PracticaMVC.Controllers
+{
+    public class InicioController : Controller
+    {
+        private readonly equiposDbContext _context;
+
+        private IConfiguration _configuration;
+
+        public InicioController (equiposDbContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+        public IActionResult Index()
+        {
+            correo enviarCorreo = new correo(_configuration);
+
+            enviarCorreo.enviar("walter.gonzalez3@catolica.edu.sv",
+                                "Prueba Asunto",
+                                "Esta es una prueba de correo desde ASP.Net");
+            return View();
+        }
+    }
+}
